c4a4c70811e4720b1469f52c22185e2cfbcb8c79
Santiago Puerta <puertasantiago2002@gmail.com>
2021-07-02 20:49:34 -0500

    cambio analisis de sentimientos aplicacion Spotify de la App Store

--- sentiment analysis/sentiment.py
+++ sentiment analysis/sentiment.py
@@ -6,10 +6,33 @@
 
 
 from textblob import TextBlob #sentiment analysis library
+import matplotlib.pyplot as plt #graphs
 
 
-def analysis(paragraph):
-    text = TextBlob(paragraph)
-    english = text.translate(to="en") #article given is Spanish I've to transalte it to english
-    #print(english) #print to see the translation
-    print(english.sentiment,"\n") #print to see the sentiment analysis
+def graphs(polarity_l:list, subjectivity_l:list):
+    try:
+        plt.plot(polarity_l, c = 'blue', label= "polarity") #graph polarity with blue
+        plt.plot(subjectivity_l, c = "orange", label = "subjectivity") #graph subjectivity with orange
+        plt.legend()
+        plt.show() #showing the graph
+    except ValueError as ve:
+        print(ve)
+
+
+def analysis(comments:list):
+    polarity_l = [] #list of given polarities from the comments
+    subjectivity_l = [] #list of given subjectivities from the comments
+    try:
+        for comment in range(0,len(comments)):
+            text = TextBlob(comments[comment])
+            polarity = text.sentiment.polarity #polarity of the comment
+            subjectivity = text.sentiment.subjectivity #subjectivity of the comment
+            #english = text.translate(to="en") # if article is given in Spanish I've to transalte it to english
+            #print(english) #print to see the translation
+            polarity_l.append(polarity) #add polarity to list
+            subjectivity_l.append(subjectivity) #add subjectivity to list
+            print(f"Comment number {comment + 1} \nPolarity analysis: {polarity}" + 
+                f"\nSubjectivity analysis: {subjectivity}") ##print to see the sentiment analysis
+        return polarity_l, subjectivity_l
+    except ValueError as ve:
+        print(ve)
